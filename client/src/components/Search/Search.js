2074777f81da9abaf07f06c777d3818f80540e57
kheersagar <santparja@gmail.com>
2022-03-11 22:29:07 +0530

    merged seacrh buttton and input field

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -8,7 +8,7 @@ function Search() {
   return (
     <div className='search_input_div'>
       <input type='text' placeholder='search..' className='search_input'/>
-      <Button variant="primary" size='lg' >search</Button>
+      <Button variant="primary" size='lg' className='search_btn'>search</Button>
     </div>
   )
 }
